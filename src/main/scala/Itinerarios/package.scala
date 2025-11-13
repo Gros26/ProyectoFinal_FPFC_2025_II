45484cdbf1152b6b234314c152b3f33934d86aaa
zoikle <134870626+Gros26@users.noreply.github.com>
2025-11-13 12:12:33 -0500

    haciendo una prueba

--- src/main/scala/Itinerarios/package.scala
+++ src/main/scala/Itinerarios/package.scala
@@ -2,6 +2,7 @@ import Datos._
 
 package object Itinerarios {
   def itinerarios(vuelos: List[Vuelo], aeropuertos: List[Aeropuerto]):(String,String) => List[Itinerario] = {
-    
+
+    //probando
   }
 }
