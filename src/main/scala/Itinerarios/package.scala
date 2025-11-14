b9744bac4d59425ebfc615f2a05606b2a177f75d
zoikle <134870626+Gros26@users.noreply.github.com>
2025-11-13 20:00:41 -0500

    add itinerarios function, pending testing

--- src/main/scala/Itinerarios/package.scala
+++ src/main/scala/Itinerarios/package.scala
@@ -1,8 +1,23 @@
 import Datos._
 
 package object Itinerarios {
-  def itinerarios(vuelos: List[Vuelo], aeropuertos: List[Aeropuerto]):(String,String) => List[Itinerario] = {
+  def itinerarios(vuelos: List[Vuelo], aeropuertos: List[Aeropuerto]): (String, String) => List[Itinerario] = {
 
-    //probando2
+    val vuelosPorOrigen = vuelos.groupBy(_.Org).withDefaultValue(Nil)
+
+    def buscar(actual: String, destino: String, visitados: Set[String]): List[Itinerario] = {
+      
+      if (actual == destino) List(Nil)
+      else {
+        val vuelosDisponibles = vuelosPorOrigen(actual).filterNot(v => visitados(v.Dst))
+        
+        for {
+          vuelo <- vuelosDisponibles
+          itinerario <- buscar(vuelo.Dst, destino, visitados + vuelo.Dst)
+        } yield vuelo :: itinerario
+      }
+    }
+
+    (cod1: String, cod2: String) => buscar(cod1, cod2, Set(cod1))
   }
 }
